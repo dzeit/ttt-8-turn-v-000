01ba882040899e5f08fac12ec38f73d99f5051a3
David Zeit <dzeit216@gmail.com>
2018-07-27 18:07:16 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def position_taken?(board, index)
   board[index] != " "
 end
 
-def valid_move?(index)
+def valid_move?(board, index)
   if index.between?(0-8) && !position_taken?(board, index)
     true
   end
