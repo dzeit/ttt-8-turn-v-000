c7012ba88cc66befbffd18f14a66114003d670fd
David Zeit <dzeit216@gmail.com>
2018-07-27 17:36:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,7 +9,7 @@ end
 
 def input_to_index(user_input)
   user_input.to_i - 1
-end 
+end
 
 def position_taken?(board, index)
   board[index] != " "
@@ -19,5 +19,6 @@ def valid_move?(board, index)
   index.between?(0-8) && !position_taken?(board, index)
 end
 
-def move
-end 
+def move(board, index, current_player = 'X')
+  board[index] = current_player
+end
